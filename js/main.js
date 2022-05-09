84a9dc69e7545111dcb35c3210748ab0ff638069
Gh0st184 <tu5s0qg>
2022-05-09 16:17:55 +0300

    'lesson-3'

--- js/main.js
+++ js/main.js
@@ -1,52 +1,68 @@
+let API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/'
+
+// let getRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('GET', url, true)
+//         xhr.onreadystatechange = () => {
+//             if (xhr.readyState === 4) {
+//                 console.log('2')
+//                 if (xhr.status !== 200) {
+//                     reject('Error');
+//                 } else {
+//                     resolve(xhr.responseText);
+//                 }
+//             }
+//         }
+//     });
+// };
+
 class ProductList {
-  constructor(container = '.products') {
-    this.container = document.querySelector(container);
-    this.goods = [];
-    this.productObjects = [];
+    constructor(container = '.products') {
+        this.container = document.querySelector(container);
+        this.goods = [];
+        this.productObjects = [];
 
-    this.fetchGoods();
-    this.render();
-    this.totalCost()
-  }
-
-  fetchGoods() {
-    this.goods = [
-      {id: 1, title: 'Notebook', price: 20000},
-      {id: 2, title: 'Mouse', price: 1500},
-      {id: 3, title: 'Keyboard', price: 5000},
-      {id: 4, title: 'Gamepad', price: 4500},
-    ];
-  }
-
-  render() {
-    for (const good of this.goods) {
-      const productObject = new ProductItem(good);
-      console.log(productObject);
-      this.productObjects.push(productObject);
-      this.container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+        this.getProducts().then((data) => {
+            this.goods = data;
+            this.render();
+            this.totalCost();
+        });
     }
-  }
 
-  totalCost() {
-    let sum = 0;
-    for (const cost of this.productObjects ) {
-      sum += cost.price
+    getProducts() {
+        return fetch(`${API}catalogData.json`).then(response => response.json()).catch(err => console.log(err));
     }
-    console.log(sum)
-    return sum;
-  }
+
+    render() {
+        for (const good of this.goods) {
+            const productObject = new ProductItem(good);
+            console.log(productObject);
+            this.productObjects.push(productObject);
+            this.container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+        }
+    }
+
+    totalCost() {
+        let sum = 0;
+        for (const cost of this.productObjects) {
+            sum += cost.price
+        }
+        console.log(sum)
+        return sum;
+    }
 }
 
 class ProductItem {
-  constructor(product, img='https://via.placeholder.com/200x150') {
-    this.id = product.id;
-    this.title = product.title;
-    this.price = product.price;
-    this.img = img;
-  }
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.id = product.id_product;
+        this.title = product.product_name;
+        this.price = product.price;
+        this.img = img;
+    }
 
-  getHTMLString() {
-    return `<div class="product-item" data-id="${this.id}">
+    getHTMLString() {
+        return `<div class="product-item" data-id="${this.id}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
                     <h3>${this.title}</h3>
@@ -54,22 +70,22 @@ class ProductItem {
                     <button class="buy-btn">Купить</button>
                 </div>
             </div>`;
-  }
+    }
 }
 
 class Basket extends ProductList {
-  constructor(container = '.basket', goods, productObjects) {
-    super(container, goods, productObjects)
-  }
-
-  delete(productItem) {
-    for (const product of this.productObjects) {
-      if (productItem.id === product.id) {
-        let index = this.productObjects.indexOf(product);
-        this.productObjects.splice(index, 1);
-      }
+    constructor(container = '.basket', goods, productObjects) {
+        super(container, goods, productObjects)
     }
-  }
+
+    delete(productItem) {
+        for (const product of this.productObjects) {
+            if (productItem.id === product.id) {
+                let index = this.productObjects.indexOf(product);
+                this.productObjects.splice(index, 1);
+            }
+        }
+    }
 }
 
 new ProductList();
